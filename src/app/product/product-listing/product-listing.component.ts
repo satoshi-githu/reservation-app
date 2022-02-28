d6b4b3a0cafc42c3426dafc0179f33df80b60395
satoshi-githu <rie1366@yahoo.co.jp>
2022-03-01 02:14:16 +0900

    frontend finish

--- src/app/product/product-listing/product-listing.component.ts
+++ src/app/product/product-listing/product-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from 'src/app/products';
 
 @Component({
   selector: 'app-product-listing',
@@ -6,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-listing.component.scss']
 })
 export class ProductListingComponent implements OnInit {
-  products: any =[1,2,3,4];
+  products: any;
 
   constructor() { }
 
   ngOnInit(): void {
-
+    this.products = products;
   }
 
 }
