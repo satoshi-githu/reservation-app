267a7ab7c39d119b4ab24497efea112213afd7aa
satoshi-githu <rie1366@yahoo.co.jp>
2022-02-28 23:40:47 +0900

    ng-for

--- src/app/product-listing/product-listing.component.ts
+++ src/app/product-listing/product-listing.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-listing.component.scss']
 })
 export class ProductListingComponent implements OnInit {
-
+  products: any =[1,2,3,4];
   constructor() { }
 
   ngOnInit(): void {
