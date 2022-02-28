8010efa85a12c7b6d8fa3f94c56685718427be34
satoshi-githu <rie1366@yahoo.co.jp>
2022-02-28 18:48:20 +0900

    product-listingページ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductListingComponent
   ],
   imports: [
     BrowserModule,
